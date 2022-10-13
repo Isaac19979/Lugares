cf0239f7af7863bea9500c1646d3ff8abdc4092b
Isaac Vargas <isaac19979@gmail.com>
2022-10-13 16:56:06 -0600

    cuarta semana

--- app/src/main/java/com/example/lugares_j/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/example/lugares_j/ui/lugar/LugarFragment.kt
@@ -9,7 +9,9 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lugares_j.R
+import com.example.lugares_j.adapter.LugarAdapter
 import com.example.lugares_j.databinding.FragmentLugarBinding
 import com.example.lugares_j.viewmodel.LugarViewModel
 
@@ -24,12 +26,22 @@ class LugarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val lugarViewModel =
-            ViewModelProvider(this).get(LugarViewModel::class.java)
+            ViewModelProvider(this)[LugarViewModel::class.java]
 
         _binding = FragmentLugarBinding.inflate(inflater, container, false)
 
         binding.addLugarFabButton.setOnClickListener {
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment)
+        }
+
+        //Se genera el recicler vie para ver la indormación...
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.reciclador
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel.getLugares.observe(viewLifecycleOwner){
+            lugares -> lugarAdapter.setListaLugares(lugares)
         }
 
         return binding.root
