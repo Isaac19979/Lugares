c904eee02f6b924295cf2f5127c45adeac8bf28b
Isaac Vargas <isaac19979@gmail.com>
2022-12-02 12:23:49 -0600

    decimosegunda semana

--- app/src/main/java/com/example/lugares_j/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/lugares_j/ui/gallery/GalleryFragment.kt
@@ -8,32 +8,71 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.lugares_j.databinding.FragmentGalleryBinding
+import com.example.lugares_j.model.Lugar
 import com.example.lugares_j.viewmodel.GalleryViewModel
+import com.example.lugares_j.viewmodel.LugarViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class GalleryFragment : Fragment() {
+class GalleryFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentGalleryBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    //Un objeto para interacturar con la vista  -mapa-
+    private lateinit var googleMap: GoogleMap
+    private var mapReady = false;
+
+    //Se utiliza lugarViewmOdel donde está el arrayList de lugares
+    private lateinit var lugarViewModel: LugarViewModel
+
+    //Programar la función para solicitar la "descarga" del objeto mapa
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) { //Sirve para descargar o atualizar el mapa
+        super.onActivityCreated(savedInstanceState)
+        binding.map.onCreate(savedInstanceState)
+        binding.map.onResume()
+        binding.map.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {//se ejecuta cuando el mapa se descarga
+        map.let {
+            googleMap = it
+            mapReady = true
+
+            //Se recorre el arreglo para dibujar los lugares
+            lugarViewModel.getLugares.observe(viewLifecycleOwner) { lugares ->
+                updatedMap(lugares)
+            }
+        }
+    }
+
+    private fun updatedMap(lugares: List<Lugar>) {
+        if(mapReady){
+            lugares.forEach { lugar ->
+                if(lugar.latitud?.isFinite()==true && lugar.longitud?.isFinite() == true) {
+                    val marca =LatLng(lugar.latitud,lugar.longitud)
+                    googleMap.addMarker(MarkerOptions().position(marca).title(lugar.nombre))
+                }
+            }
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        lugarViewModel =
+            ViewModelProvider(this)[lugarViewModel::class.java]
 
         _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+
+
+        return binding.root
     }
 
     override fun onDestroyView() {
@@ -41,3 +80,4 @@ class GalleryFragment : Fragment() {
         _binding = null
     }
 }
+
